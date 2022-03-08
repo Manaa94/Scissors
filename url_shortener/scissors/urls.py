9bf6b76f7364bbd6d48a20015d8648cf4fd72eba
mana <manashams94@gmail.com>
2022-03-08 17:33:46 +0330

    UrlCreateView & its path added.

--- url_shortener/scissors/urls.py
+++ url_shortener/scissors/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UrlListView
+from .views import UrlListView, UrlCreateView
 
-
+app_name = 'scissors'
 urlpatterns = [
     path('', UrlListView.as_view(), name='list-url'),
+    path('url/', UrlCreateView.as_view(), name='create-url'),
+
 ]
--- url_shortener/scissors/views.py
+++ url_shortener/scissors/views.py
@@ -1,8 +1,23 @@
-from django.views.generic import list
+from django.views.generic import list, edit
+from django.urls import reverse
 from .models import Url
+from .forms import UrlForm
 
 
 class UrlListView(list.ListView):
     model = Url
     template_name = 'index.html'
     queryset = Url.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UrlForm
+        return context
+
+
+class UrlCreateView(edit.CreateView):
+    form_class = UrlForm
+    template_name = 'index.html'
+
+    def get_success_url(self):
+        return reverse('scissors:list-url')
