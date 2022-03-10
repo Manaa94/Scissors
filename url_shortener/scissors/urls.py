479ac45f01afc20c00fa565773cb3bcb30fb1224
mana <manashams94@gmail.com>
2022-03-10 09:18:37 +0330

    UrlCopyPathView & and its url added - html fixed.

--- url_shortener/scissors/urls.py
+++ url_shortener/scissors/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UrlListView, UrlCreateView, UrlRedirectView
+from .views import UrlListView, UrlCreateView, UrlRedirectView, UrlCopyPathView
 
 app_name = 'scissors'
 urlpatterns = [
     path('', UrlListView.as_view(), name='list-url'),
     path('url/', UrlCreateView.as_view(), name='create-url'),
-    path('<str:path>/', UrlRedirectView.as_view(), name='redirect-url'),
+    path('s/<str:path>/', UrlRedirectView.as_view(), name='redirect-url'),
+    path('<int:pk>/', UrlCopyPathView.as_view(), name='copy-url'),
 ]
--- url_shortener/scissors/views.py
+++ url_shortener/scissors/views.py
@@ -1,18 +1,20 @@
 from django.views.generic import list, edit, base
+from django.http import HttpResponseRedirect
+from django.views import View
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from .models import Url
 from .forms import UrlForm
+import pyperclip
 
 
 class UrlListView(list.ListView):
-    model = Url
     template_name = 'index.html'
     queryset = Url.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = UrlForm
+        context['form'] = UrlForm()
         return context
 
 
@@ -28,3 +30,10 @@ class UrlRedirectView(base.RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         url = get_object_or_404(Url, path=kwargs['path'])
         return url.original
+
+
+class UrlCopyPathView(View):
+    def get(self, request, pk):
+        path = get_object_or_404(Url, pk=pk).path
+        pyperclip.copy(f'/s/{path}')
+        return HttpResponseRedirect(reverse('scissors:list-url'))
