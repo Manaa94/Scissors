c845ea5f1d212c54c28afa1192ecdd5c8995c0e2
mana <manashams94@gmail.com>
2022-03-10 10:59:01 +0330

    UrlEditPathView's url added.

--- url_shortener/scissors/urls.py
+++ url_shortener/scissors/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import UrlListView, UrlCreateView, UrlRedirectView, UrlCopyPathView
+from .views import UrlListView, UrlCreateView, UrlRedirectView, UrlCopyPathView, UrlEditPathView
 
 app_name = 'scissors'
 urlpatterns = [
     path('', UrlListView.as_view(), name='list-url'),
     path('url/', UrlCreateView.as_view(), name='create-url'),
     path('s/<str:path>/', UrlRedirectView.as_view(), name='redirect-url'),
-    path('<int:pk>/', UrlCopyPathView.as_view(), name='copy-url'),
+    path('copy/<int:pk>/', UrlCopyPathView.as_view(), name='copy-url'),
+    path('<int:pk>/', UrlEditPathView.as_view(), name='edit-url'),
+
 ]
